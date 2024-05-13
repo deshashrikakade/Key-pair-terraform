b729d4f0383299c6513746f096220123f0487d86
deshashrikakade <119559216+deshashrikakade@users.noreply.github.com>
2024-05-13 16:51:15 +0530

    security-group.tf

---
+++ security-group.tf
@@ -0,0 +1,57 @@
+resource "aws_instance" "web" {
+  ami           = "ami-07caf09b362be10b8"       
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.TF_SG.name]
+  key_name = "demo"
+  
+  tags = {
+    Name = "Terraform Ec2"
+  }
+}
+
+#securitygroup using Terraform
+
+resource "aws_security_group" "TF_SG" {
+  name        = "security group using Terraform"
+  description = "security group using Terraform"
+  vpc_id      = "vpc-0f17c4e9d992b5d80 "
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443te
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "TF_SG"
+  }
+}
